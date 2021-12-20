f3f60260f332c9b0bf41b6abba172ded37fd79f3
alex <example@gmail.com>
2021-12-20 11:02:45 +0500

    new commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+// console.log( 2*4 == '8');
+// console.log( 2*4 || '8');
+
+const  num = 50; 
+// if (num < 49){
+//     console.log('Eror')
+// }
+(num == 50) ? console.log('Ok') : console.log('eror');
